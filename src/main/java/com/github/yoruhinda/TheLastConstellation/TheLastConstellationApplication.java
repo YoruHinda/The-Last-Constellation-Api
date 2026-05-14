a80d0b262b2335b65c15ab7255d6205a3ba1b045
Ruan Phablo <ruanphablo778@gmail.com>
2026-05-13 23:06:43 -0300

    First Commit!

---
+++ src/main/java/com/github/yoruhinda/TheLastConstellation/TheLastConstellationApplication.java
@@ -0,0 +1,13 @@
+package com.github.yoruhinda.TheLastConstellation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheLastConstellationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheLastConstellationApplication.class, args);
+	}
+
+}
