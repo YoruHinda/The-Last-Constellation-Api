ea04118ba4f8e7597b1c0332031bb669b6eb859a
Ruan Phablo <ruanphablo778@gmail.com>
2026-05-25 22:48:28 -0300

    Feat: Update Wish Model!

--- src/main/java/com/github/yoruhinda/TheLastConstellation/model/Wish.java
+++ src/main/java/com/github/yoruhinda/TheLastConstellation/model/Wish.java
@@ -1,4 +1,19 @@
 package com.github.yoruhinda.TheLastConstellation.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class Wish {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String text;
+    private String category;
+    private String completed;
+    private LocalDateTime createdAt;
 }
